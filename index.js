ad515c93d5cc1a9e6f6d652815be7be18612b4b8
Seven <graf.lancelot7@gmail.com>
2023-05-12 11:12:28 +0300

    Update index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const btnEl = document.querySelector(".login-form-item-start-btn");
-btnEl.style.display = "none";
-setInterval(()=>{
-    console.log('second')
-},1000)
-setTimeout(()=>{
-    btnEl.style.display = "block";
-    console.log('end')
-},5000)
